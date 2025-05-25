d3fccd19c83432714672d045cbfd462e1f7ea6b5
BilliBoleMeow <88665437+BilliBoleMeow@users.noreply.github.com>
2025-05-25 14:16:46 +0530

    Update telegram_uploader.py

--- bot/helper/mirror_leech_utils/telegram_uploader.py
+++ bot/helper/mirror_leech_utils/telegram_uploader.py
@@ -220,7 +220,6 @@ class TelegramUploader:
 
     async def upload(self):
         await self._user_settings()
-            return
         for dirpath, _, files in natsorted(await sync_to_async(walk, self._path)):
             if dirpath.strip().endswith("/yt-dlp-thumb"):
                 continue
